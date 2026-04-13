55b15be51a431da616fb53909658efc5bf9fcfbd
diegoifsmg365 <diegoifsmg365@gmail.com>
2026-04-13 16:50:18 -0300

    Add files via upload

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
 from .models import *
 
+class PessoaInline(admin.TabularInline):
+    model = Pessoa
+    extra = 1
+
+class OcupacaoAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    inlines = [PessoaInline]
+
+class CursoInline(admin.TabularInline):
+    model = Curso
+    extra = 1
+
+class InstituicaoEnsinoAdmin(admin.ModelAdmin):
+    inlines = [CursoInline]
+
 admin.site.register(Cidade)
-admin.site.register(Ocupacao)
+admin.site.register(Ocupacao, OcupacaoAdmin)
 admin.site.register(Pessoa)
-admin.site.register(InstituicaoEnsino)
+admin.site.register(InstituicaoEnsino, InstituicaoEnsinoAdmin)
 admin.site.register(AreaSaber)
 admin.site.register(Curso)
 admin.site.register(Turma)
